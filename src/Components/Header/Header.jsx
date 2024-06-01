d8e8563b82f154f958d22940b1ffccfc1c51c2cc
xkl <joselucas20048jl@gmail.com>
2024-06-01 12:47:00 -0300

    header updates

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import rnm from '../../assets/rnm.png';
 
 export const Header = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
-    <div className="bg-gradient-to-w-full h-16 flex items-center justify-between px-4   shadow-lg shadow-black-500/40">
+    <div className="bg-gradient-to-r from-blue-500 to-indigo-600 h-16 flex items-center justify-between px-4 shadow-lg rounded-b-lg">
       <div className="flex items-center space-x-2">
-        <img className="w-8 h-8" src={rnm} alt="Rick and Morty logo" />
+        <img
+          className="w-8 h-8 rounded-full cursor-pointer"
+          src={rnm}
+          alt="Rick and Morty logo"
+          onClick={toggleModal}
+        />
         <h1 className="text-lg font-semibold text-white">Rick and Morty API</h1>
       </div>
-      <ul className="flex items-center space-x-4 [&>li]:text-[white]: ">
-        <li className=" hover:text-white transition-colors duration-300 cursor-pointer">Home</li>
-        <li className=" hover:text-white transition-colors duration-300 cursor-pointer">About</li>
-        <li className=" hover:text-white transition-colors duration-300 cursor-pointer">FAQ</li>
-        <li className="hover:text-white transition-colors duration-300 cursor-pointer">Contact</li>
-      </ul>
+      {openModal && (
+        <div className="absolute top-full right-0 transform translate-x-1/2 -translate-y-1/2 p-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-lg font-semibold text-gray-800 mb-4">Modal Title</h2>
+          <p className="text-gray-700 mb-4">Here are some information:</p>
+          <ul className="text-gray-700 mb-4">
+            <li>Contact: example@example.com</li>
+            <li>Settings: Lorem ipsum dolor sit amet.</li>
+          </ul>
+          <button
+            onClick={toggleModal}
+            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-400"
+          >
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 };
